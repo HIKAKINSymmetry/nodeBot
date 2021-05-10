75d13319a14bc8a1a83bc447ff88b5b5d2ea2e0b
huequica <harumaki1225@gmail.com>
2021-05-11 00:05:56 +0900

    ルールいくつか追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
+	env: {
+		node: true
+	},
+
   root: true, // .eslintrc.jsがプロジェクトのルートに配置させているか（指定がないと上位階層へ設定ファイルを探索される）
   parser: '@typescript-eslint/parser', // ESLintにTypeScriptを理解させる
   parserOptions: {
@@ -13,4 +17,15 @@ module.exports = {
     'plugin:@typescript-eslint/recommended', // 型チェックが不要なルールを適用
     'plugin:@typescript-eslint/recommended-requiring-type-checking', // 型チェックが必要なルールを適用
   ],
+	rules: {
+		semi: 'error',
+		'no-var': 'error',
+		eqeqeq: 'error',
+		'comma-spacing': 'error',
+		indent: ['error', 'tab'],
+		'block-spacing': 'error',
+		'space-infix-ops': 'error',
+		'arrow-spacing': 'error',
+		'no-mixed-spaces-and-tabs': 'error'
+	}
 }
