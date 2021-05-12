e4edf7e6e4a71b1670c7a20610c099a376902e34
huequica <harumaki1225@gmail.com>
2021-05-13 02:00:17 +0900

    シンメトリー画像を作るプロセスを実装

---
+++ src/modules/makeSymmetryImages.ts
@@ -0,0 +1,89 @@
+import vision from '@google-cloud/vision';
+import EnvYaml from './envReader';
+import SymmetryImage from './generateImages';
+import path from 'path';
+
+/**
+ * 顔の四編の頂点情報から中心のX座標を算出する
+ * @param {fdBoundingPoly.vertices} fdVertices 顔の検出した四編の頂点座標群
+ * @returns {number} 中心のX座標の数値
+ */
+const detectFacesCenterCoordinate = (fdVertices: fdBoundingPoly.vertices): number	 => {
+	const fdPositionsXCoordinates = fdVertices.map((fdVertice) => {
+		return fdVertice.x;
+	});
+
+	// 配列の中の最大値と最小値を先に出しておく
+	const maxCoordinate = Math.max(...fdPositionsXCoordinates);
+	const minCoordinate = Math.min(...fdPositionsXCoordinates);
+
+	// 小数点があるとエラーになりそうなので小数点以下を切り捨てる
+	return Math.floor((maxCoordinate + minCoordinate) / 2);
+
+};
+
+/**
+ * 画像をvisionAPIにかけ, 顔が検出されればシンメトリー画像を作って
+ * 1ツイートごとに `Array` に内包して返す
+ * @param {string} filePath シンメトリー画像を作る元の画像
+ * @return {tweet.containMedia[]} 顔が見つかればツイートに使う画像のファイルパス群
+ * 																なければ空の配列
+ */
+const generateTweetsImage = async (filePath: string): Promise<tweet.containMedia[]> => {
+	const visionAPI = new vision.ImageAnnotatorClient({
+		projectId: EnvYaml.GCPProjectID(),
+		// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+		keyFilename: `${process.env.PWD}/.envs/serviceAccount.json`
+	});
+	const [faceDetectionResult] = await visionAPI.faceDetection(filePath);
+	const faces = faceDetectionResult.faceAnnotations;
+
+	return new Promise((Resolve) => {
+
+		// `undefined` と `null` は〇す
+		if(typeof faces !== 'undefined' && faces !==  null){
+
+			// 顔が検出されない場合があるので確認を通す
+			if(faces.length > 0){
+				// ファイルの名前(拡張子なし)を取る(生成する画像のファイル名の決定に使う)
+				const originalImageBaseFilename = path.basename(filePath, path.extname(filePath));
+
+				const tweets = faces.map((face, currentNumber): Promise<tweet.containMedia> => {
+					return new Promise((resolve) => {
+						const tweetMediaList: tweet.containMedia = [];
+						// 顔の中心座標の算出
+						const FaceCenterCoordinate = detectFacesCenterCoordinate(
+							face.fdBoundingPoly?.vertices as fdBoundingPoly.vertices
+						);
+
+						// 2枚の画像の生成を平行して実行、全部終わったらファイルパス群が帰ってくる
+						void Promise.all([
+							SymmetryImage.make1stImage(filePath, FaceCenterCoordinate, `${originalImageBaseFilename}_${currentNumber}_1.jpg`),
+							SymmetryImage.make2ndImage(filePath, FaceCenterCoordinate, `${originalImageBaseFilename}_${currentNumber}_2.jpg`),
+						])
+							.then((generateFilePaths) => {
+								// ツイートの元画像を追加してからシンメトリー画像を突っ込む
+								tweetMediaList.push(filePath);
+								generateFilePaths.forEach((generateFilepath) => tweetMediaList.push(generateFilepath));
+								resolve(tweetMediaList);
+							});
+
+					});
+				});
+
+				void Promise.all(tweets).then((hoge) => {
+					Resolve(hoge);
+				});
+			}
+			else{
+				// 見つからなければ特に何もしない
+				console.log('画像から顔が検出されませんでした');
+				Resolve([]);
+
+			}
+		}
+
+	});
+};
+
+export default generateTweetsImage;
