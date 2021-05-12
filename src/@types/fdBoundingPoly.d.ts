def9da3a3ea9ea7b2901d178d63f1e03c6ad95b0
huequica <harumaki1225@gmail.com>
2021-05-12 23:58:51 +0900

    visionAPIの `fdBoundingPoly.vertices` の型が取れなさそうなので内製

---
+++ src/@types/fdBoundingPoly.d.ts
@@ -0,0 +1,14 @@
+declare namespace fdBoundingPoly{
+	/**
+	 * 顔の四編の頂点座標
+	 */
+	export type vertex = {
+		x: number,
+		y: number
+	};
+
+	/**
+	 * 顔の矩形の頂点群
+	 */
+	export type vertices = vertex[];
+}
