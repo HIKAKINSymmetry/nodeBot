6e2a2a732329717ffd95727e218f7116e4913413
huequica <harumaki1225@gmail.com>
2021-05-13 16:09:44 +0900

    Twitterの投稿周りの関数を実装

--- src/symmetryYoutubeThumbs.ts
+++ src/symmetryYoutubeThumbs.ts
@@ -6,6 +6,7 @@ import generateTweetsImage from './modules/makeSymmetryImages';
 
 //    型の名前空間   実際のクラス
 import FireStore, { Firestore } from '@google-cloud/firestore';
+import twit from 'twit';
 import path from 'path';
 import fs from 'fs';
 
@@ -30,6 +31,72 @@ const findHaventProcessedVideo = async (db: FireStore.Firestore) : Promise<DB.Mo
 	return haventProcessedVideos;
 };
 
+/**
+ * ファイルパスに指定した画像をTwitterのmediaサーバにアップロードする
+ * @param {twit} twAPI `twit` のインスタンス
+ * @param Image 画像のファイルパス
+ * @returns {Promise<string | Error>} 成功していればメディアのID(string)
+ */
+const uploadImage = (twAPI: twit, Image: string): Promise<string | Error>  => {
+	const encodedImage = fs.readFileSync(Image, {encoding: 'base64'});
+	return new Promise((resolve, reject) => {
+		twAPI.post('media/upload', {media_data: encodedImage}, (error, result) => {
+			const typedResult = result as tweet.mediumUploadResponse;
+			if(error) reject(error);
+			resolve(typedResult.media_id_string);
+		});
+	});
+};
+
+/**
+ * Twitterに画像付きツイートをアップロードする
+ * @param {tweet.containMedia} tweetImages 1ツイート分の画像のファイルパス
+ * @returns {Promise<true>} 全プロセスで成功すれば `true` そうでなければrejectされてる
+ */
+const updateTwitterStatus = (tweetImages: tweet.containMedia): Promise<true> => {
+	const config = EnvYaml.TwitterAPI();
+	const twitterAPI = new twit({
+		consumer_key: config.consumer.key,
+		consumer_secret: config.consumer.secret,
+		access_token: config.access.token,
+		access_token_secret: config.access.secret
+	});
+
+	const postImageToTwitter = tweetImages.map((containImage) => uploadImage(twitterAPI, containImage));
+	return new Promise((resolve, reject) => {
+
+		void Promise.all(postImageToTwitter).then((postedMediaIds) => {
+			const filteredMediaIds = postedMediaIds.filter((currentItem): currentItem is string => typeof currentItem === 'string');
+
+			// Error が混ざってて捨てられたものがある場合要素数に変化があるのでそれを見る
+			if(filteredMediaIds.length !== postedMediaIds.length){
+				// エラーが混ざってたことになるので棄却
+				console.log('画像のアップロード中にエラーが発生しました');
+				reject();
+			}
+			else {
+				// 問題なくメディアがアップロードできているので Statuses/update をする
+				void twitterAPI.post('statuses/update', {status: '', media_ids: filteredMediaIds}, (error) => {
+					if(error){
+						console.log('投稿に失敗しました');
+						reject();
+					}
+					else {
+						console.log('投稿に成功しました');
+						resolve(true);
+					}
+				});
+			}
+
+		});
+
+	});
+};
+
+/**
+ * 動画の情報からシンメトリー画像を生成し、Twitterにアップロードする
+ * @param {DB.MovieDetail} video 1つの動画情報の集合オブジェクト
+ */
 const makeSymmetryTweet = async (video: DB.MovieDetail) => {
 	if(typeof video.thumb !== 'undefined'){
 		const thumbImageExtention = path.extname(video.thumb);
