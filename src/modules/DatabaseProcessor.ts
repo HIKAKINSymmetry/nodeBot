83da9fe041dd665a5670f12f4628af76af52390f
huequica <harumaki1225@gmail.com>
2021-05-13 23:31:13 +0900

    単一の動画情報をFirestoreにpushする関数を作った

--- src/modules/DatabaseProcessor.ts
+++ src/modules/DatabaseProcessor.ts
@@ -21,4 +21,22 @@ const putMovies = (movies: Array<DB.MovieDetail>): void => {
 	});
 };
 
-export {putMovies};
+/**
+ * 1つの動画オブジェクトをFirestoreにpushする
+ * @param movies 単一の動画
+ */
+const putMovie = (movie: DB.MovieDetail): void => {
+
+	const fireStoreCredentials: FireStore.Settings = {
+		projectId: envYaml.GCPProjectID(),
+		// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+		keyFilename: `${process.env.PWD}/.envs/serviceAccount.json`
+	};
+	const database = new Firestore(fireStoreCredentials);
+
+	console.log(`データベースに記述中 : ${movie.videoId}/${movie.title} `);
+	void database.collection('movies').doc().set(movie);
+
+};
+
+export {putMovies, putMovie};
