1bdf84478768fa1eeb85d33198b05fc7591a1371
huequica <harumaki1225@gmail.com>
2021-05-13 15:17:02 +0900

    twitterのmediaをアップロードした際のレスポンスの型を内製した

--- src/@types/tweetMedia.d.ts
+++ src/@types/tweetMedia.d.ts
@@ -3,4 +3,21 @@ declare namespace tweet{
 	 * 1ツイート内に収める画像群(画像はファイルパスで表現)
 	 */
 	export type containMedia = string[];
+
+	/**
+	 * Twitterのメディアサーバにアップロードした時のレスポンス
+	 * https://developer.twitter.com/en/docs/twitter-api/v1/media/upload-media/api-reference/post-media-upload
+	 */
+	export type mediumUploadResponse = {
+		media_id: number,
+		media_id_string: string,
+		media_key: string,
+		size: number,
+		expires_after_secs: number,
+		image: {
+			image_type: string,
+			w: number,
+			h: number
+		}
+	}
 }
