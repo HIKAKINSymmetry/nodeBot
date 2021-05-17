c61db9df1a7f08ec130ef1e0a041b40385541e71
huequica <harumaki1225@gmail.com>
2021-05-17 12:24:36 +0900

    Twitter監視のエントリーポイントを実装　これで何時まで持つかは知らん

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import Cron from 'node-cron';
 import symmetryYoutubeThumb from './symmetryYoutubeThumbs';
-
+import symmetryTwitterImages from './symmetryTwitterImages';
 Cron.schedule('* */10 * * *', () => {
 	// console.log('10分おき実行');
 	void symmetryYoutubeThumb();
 });
+
+void symmetryTwitterImages();
