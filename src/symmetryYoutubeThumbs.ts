3ba6b25b432ea8f7a2fdebba41516c7af19c16de
huequica <harumaki1225@gmail.com>
2021-05-17 11:58:43 +0900

    Twitterのアップロード部分を共用するので切り分け

--- src/symmetryYoutubeThumbs.ts
+++ src/symmetryYoutubeThumbs.ts
@@ -3,11 +3,10 @@ import EnvYaml from './modules/envReader';
 import {putMovies, putMovie} from './modules/DatabaseProcessor';
 import downloadImage from './modules/downloadImage';
 import generateTweetsImage from './modules/makeSymmetryImages';
+import updateTwitterStatus from './modules/tweetUploader';
 
 //    型の名前空間   実際のクラス
 import FireStore, { Firestore } from '@google-cloud/firestore';
-import {TwitterClient} from 'twitter-api-client';
-// import sleep from 'sleep-promise';
 import fs from 'fs';
 
 /**
@@ -31,79 +30,6 @@ const findHaventProcessedVideo = async (db: FireStore.Firestore) : Promise<DB.Mo
 	return haventProcessedVideos;
 };
 
-/**
- * 画像をTwitterのmediaサーバにアップロードする
- * @param {TwitterClient} twAPI `twitterClient` のインスタンス
- * @param encodedImage base64エンコードした画像
- * @returns {Promise<string | Error>} 成功していればメディアのID(string)
- */
-const uploadImage = (twAPI: TwitterClient, encodedImage: string): Promise<string | Error>  => {
-	return new Promise((resolve) => {
-		const uploadImage = twAPI.media.mediaUpload({
-			media_data: encodedImage,
-			media_category: 'tweet_image'
-		});
-		void uploadImage
-			.then((response) => {
-				console.log('media Upload Success', response.media_id_string);
-				resolve(response.media_id_string);
-			})
-			.catch((reason) => {
-				console.log(reason);
-			});
-	});
-};
-
-/**
- * Twitterに画像付きツイートをアップロードする
- * @param {tweet.containMedia} tweetImages 1ツイート分の画像
- * @returns {Promise<true>} 全プロセスで成功すれば `true` そうでなければrejectされてる
- */
-const updateTwitterStatus = async (tweetImages: tweet.containMedia): Promise<true> => {
-	const config = EnvYaml.TwitterAPI();
-	const twitterAPI = new TwitterClient({
-		apiKey: config.consumer.key,
-		apiSecret: config.consumer.secret,
-		accessToken: config.access.token,
-		accessTokenSecret: config.access.secret
-	});
-
-	const postImageToTwitter = tweetImages.map((containImage) => uploadImage(twitterAPI, containImage));
-	return new Promise((resolve, reject) => {
-
-		void Promise.all(postImageToTwitter)
-			.then((postedMediaIds) => {
-
-				const filteredMediaIds = postedMediaIds.filter((currentItem): currentItem is string => typeof currentItem === 'string');
-
-				// Error が混ざってて捨てられたものがある場合要素数に変化があるのでそれを見る
-				if(filteredMediaIds.length !== postedMediaIds.length){
-					// エラーが混ざってたことになるので棄却
-					console.log('画像のアップロード中にエラーが発生しました');
-					reject();
-				}
-				else {
-					// 問題なくメディアがアップロードできているので Statuses/update をする
-					void twitterAPI.tweets.statusesUpdate({
-						status: '',
-						media_ids: filteredMediaIds.join(',')
-					}).then((statusUpdateResponse) => {
-						if(statusUpdateResponse.id_str){
-							resolve(true);
-						}
-						else{
-							reject();
-						}
-					});
-				}
-
-			}).catch((error) => {
-				console.log('画像のアップロードが棄却されました');
-				console.log(error);
-			});
-
-	});
-};
 
 /**
  * 動画の情報からシンメトリー画像を生成し、Twitterにアップロードする
