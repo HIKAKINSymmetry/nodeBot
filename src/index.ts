c4cb426a1a3835510e6ddd7f133419122b3419e1
huequica <harumaki1225@gmail.com>
2021-06-03 21:03:31 +0900

    コメント部分修正

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import symmetryYoutubeThumb from './symmetryYoutubeThumbs';
 import symmetryTwitterImages from './symmetryTwitterImages';
 //  [seconds, minute, hour, day, month, dayOfWeek]
 Cron.schedule('0 */10 * * * *', () => {
-	// console.log('1時間おき実行');
+	// console.log('10分に1回実行');
 	void symmetryYoutubeThumb();
 });
 
