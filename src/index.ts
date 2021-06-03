3967edd44ebb8b3096abb5e6b82be5aff2d2d44b
huequica <harumaki1225@gmail.com>
2021-06-03 21:02:56 +0900

    Youtubeへのアクセスのタイミング設定を10分に1回に変更

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import Cron from 'node-cron';
 import symmetryYoutubeThumb from './symmetryYoutubeThumbs';
 import symmetryTwitterImages from './symmetryTwitterImages';
-//  [minute, hour, day, month, dayOfWeek]
-Cron.schedule('0 * * * *', () => {
+//  [seconds, minute, hour, day, month, dayOfWeek]
+Cron.schedule('0 */10 * * * *', () => {
 	// console.log('1時間おき実行');
 	void symmetryYoutubeThumb();
 });
